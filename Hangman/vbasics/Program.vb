8e541167d6b9d930dbc7d3f18fd0c44f2ef17d5b
Chase Young <chasekyoung@gmail.com>
2023-02-08 15:04:24 -0500

    created a hangman game

---
+++ Hangman/vbasics/Program.vb
@@ -0,0 +1,22 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Welcome to hangman! Guess the 7 letter word")
+
+        dim answer as String = ("hangman")
+        dim blank as String = ("_ _ _ _ _ _ _")
+        Console.WriteLine(blank)
+        For index as Integer = 1 To 6
+            Console.WriteLine("Enter guess: ")
+            dim guess as String = Console.ReadLine()
+            If guess = answer Then
+                Console.WriteLine("Correct! Good job!")
+                Exit For
+            Else
+                Console.WriteLine("Wrong! Try again!")
+            End If
+        Next
+        
+    End Sub
+End Module
