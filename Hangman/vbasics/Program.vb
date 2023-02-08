54544e237d235753b7577fce41d0c0e8e39c8c1a
Chase Young <chasekyoung@gmail.com>
2023-02-08 15:12:21 -0500

    changed answer

--- Hangman/vbasics/Program.vb
+++ Hangman/vbasics/Program.vb
@@ -4,7 +4,7 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Welcome to hangman! Guess the 7 letter word")
 
-        dim answer as String = ("hangman")
+        dim answer as String = ("program")
         dim blank as String = ("_ _ _ _ _ _ _")
         Console.WriteLine(blank)
         For index as Integer = 1 To 6
